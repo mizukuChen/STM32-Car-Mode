9bc0873722d06d7b77375443fb976c6aba46f7f2
mizuku <3553989549@qq.com>
2025-07-21 22:07:14 +0800

    增加了计算坐标的功能

--- Drivers/Mode/Src/Mode_encoder.c
+++ Drivers/Mode/Src/Mode_encoder.c
@@ -3,6 +3,16 @@
 #include <main.h>
 #include <Mode_encoder.h>
 
+#define ENCODER_TO_COORDINATE 1  // 实际长度与编码器数值的比值
+#define CAR_LENGTH 1
+#define ENCODER_PERIOD 8000
+
+float coordinate_x = 0;
+float coordinate_y = 0;
+float left_encoder = 0;
+float right_encoder = 0;
+float last_angle = 0;
+
 void trace_encoder(uint16_t encoder_max, float speed)
 {
 	PID TracePID;
@@ -30,3 +40,49 @@ void trace_encoder(uint16_t encoder_max, float speed)
 	}
 	return;
 }
+
+void reset_coordinates()
+{
+	left_encoder = __HAL_TIM_GET_COUNTER(&htim1);
+	right_encoder = __HAL_TIM_GET_COUNTER(&htim4);
+	last_angle = Yaw;
+}
+
+void update_coordinates()//更新坐标数值，第一次使用前需要进行复位
+{
+	float left_alter = fmod( (__HAL_TIM_GET_COUNTER(&htim1) - left_encoder), ENCODER_PERIOD ) * ENCODER_TO_COORDINATE;
+	//左轮移动距离
+	float right_alter = fmod( (__HAL_TIM_GET_COUNTER(&htim4) - right_encoder), ENCODER_PERIOD ) * ENCODER_TO_COORDINATE;
+	//右轮移动距离
+	float mid_value_alter = ( left_alter + right_alter ) /2;
+	//两轮移动距离的平均值（可认为两轮中心移动距离）
+
+	left_encoder = __HAL_TIM_GET_COUNTER(&htim1);
+	right_encoder = __HAL_TIM_GET_COUNTER(&htim4);
+	//记录本次编码器的值
+
+	float alter_angle = (right_alter - left_alter) / CAR_LENGTH / 2;
+	//两轮中心位移的相对朝向
+	float orientation_angle = (Yaw + last_angle) / 2 + alter_angle;
+	//两轮中心位移的绝对朝向（以Yaw == 0为参考方向）
+
+	last_angle = Yaw;
+	//记录本次的汽车朝向角
+
+	coordinate_x -=  mid_value_alter * cosf(orientation_angle);
+	coordinate_y -=  mid_value_alter * sinf(orientation_angle);
+	//更新坐标
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
