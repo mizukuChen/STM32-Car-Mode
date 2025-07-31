1f346d3366f4a2b788e4536a14dd910b33550b08
mizuku <3553989549@qq.com>
2025-07-31 20:58:21 +0800

    修改项目名称

--- MDK-ARM/RTE/_model/RTE_Components.h
+++ MDK-ARM/RTE/_model/RTE_Components.h
@@ -3,8 +3,8 @@
  * Auto generated Run-Time-Environment Configuration File
  *      *** Do not modify ! ***
  *
- * Project: '2021F' 
- * Target:  '2021F' 
+ * Project: 'model' 
+ * Target:  'model' 
  */
 
 #ifndef RTE_COMPONENTS_H
